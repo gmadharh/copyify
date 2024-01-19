e3b5cf01e351ad1dd70b203dc3ef3ac384dd2c8f
gevin <gmadharh@uoguelph.ca>
2024-01-19 14:19:41 -0500

    configured crx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
 // click a button
 // executes a script where it grabs the song name, artist and album
 // displays those in the text field
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <button className="">Get details</button>
     </>
   )
 }
