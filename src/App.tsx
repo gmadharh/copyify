5f850e618d78c426482806eb956f15f4d851d446
gevin <gmadharh@uoguelph.ca>
2024-01-19 15:32:31 -0500

    work on getting songname

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // click a button
 // executes a script where it grabs the song name, artist and album
 // displays those in the text field
+import './index.css'
+import run from './script.ts'
+
 function App() {
   return (
     <>
-      <button className="">Get details</button>
+      <button className="details-btn" onClick={run}>
+        Get details
+      </button>
+      <input className="text-field" type="text" readOnly></input>
     </>
   )
 }
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+export default async function run() {
+  const songTitle = document.querySelector('.main-view-container')
+
+  console.log(songTitle)
+}
