87d058155acd9ab9bc172bffe9f20ebcda315835
gevin <gmadharh@uoguelph.ca>
2024-01-25 20:27:13 -0500

    extract song,album and artist

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,32 @@
 console.log('executing script')
+type Track = {
+  songName?: string
+  artist?: string
+  albumName?: string
+}
+
+const track: Track = {}
+
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+  const songName = document.querySelector('.encore-text-headline-large')
+  const artist = document.querySelector('a[data-testid="creator-link"]')
+  const albumName = document.querySelector('span.encore-text-body-small a')
+
+  if (songName && artist && albumName) {
+    track.songName = songName.textContent ?? ''
+    track.artist = artist.textContent ?? ''
+    track.albumName = albumName.textContent ?? ''
+
+    // send message to background with track info
+    // background sends message to index.ts with track info
+    // track info gets displayed in popup html
+
+    console.log(track)
+    mutationInstance.disconnect()
+  }
+})
+
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+})
