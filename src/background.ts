ce816148cda52c08a1d00132d8da16ee83fe9ef3
gevin <gmadharh@uoguelph.ca>
2024-01-29 18:19:53 -0500

    added checking for tab navigation in service worker

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,12 @@
-//chrome.runtime.onMessage.addListener(function (sender, request, sendResponse) {
-//console.log('background.ts recieved track', request)
-//})
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log(tabId, changeInfo, tab)
+  if (
+    tab.url?.startsWith('http://open.spotify.com/track/') ||
+    tab.url?.startsWith('https://open.spotify.com/track/')
+  ) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['script.js'],
+    })
+  }
+})
