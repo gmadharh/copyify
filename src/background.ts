9043f75c95649e7dd1792a7b4aed7dadf1b9979c
gevin <gmadharh@uoguelph.ca>
2024-01-29 18:37:24 -0500

    added change info

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(tabId, changeInfo, tab)
   if (
-    tab.url?.startsWith('http://open.spotify.com/track/') ||
-    tab.url?.startsWith('https://open.spotify.com/track/')
+    changeInfo.status === 'complete' &&
+    (tab.url?.startsWith('http://open.spotify.com/track/') ||
+      tab.url?.startsWith('https://open.spotify.com/track/'))
   ) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
--- src/components/App.vue
+++ src/components/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onUpdated } from 'vue'
 import TrackInfo from './TrackInfo.vue'
 import { Track } from '../types.js'
 export default defineComponent({
