6f6b8700637fabff012bb0dabb8e18f9e0aae0a9
gevin <gmadharh@uoguelph.ca>
2024-02-01 12:19:45 -0500

    centered the heading and input justified and aligned

--- src/components/TrackInfo.vue
+++ src/components/TrackInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col justify-center items-center">
     <h3 class="text-base font-lora">{{ heading }}</h3>
     <input
       type="text"
